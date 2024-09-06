20747b6949dd72a4819d64952e2a0a6c946ac2da
Luke Encrapera <l.encrapera@gmail.com>
2024-09-06 10:38:15 -0400

    Update demo.py

--- venv/demo.py
+++ venv/demo.py
@@ -1,17 +1,18 @@
 import pytesseract
-from PIL import Image
 import cv2
+from PIL import Image
 import os
+import numpy as np
 
 # Set the path to Tesseract executable within the virtual environment
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Luke\Documents\GitHub\OCR\venv\Lib\tesseract\tesseract.exe'
 
 def ocr_core(image_path):
-    # Read the image using OpenCV
-    img = cv2.imread(image_path)
+    # Read the image using PIL
+    img = Image.open(image_path)
     
     # Convert image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2GRAY)
     
     # Use Tesseract to do OCR on the image
     text = pytesseract.image_to_string(gray)
@@ -19,6 +20,8 @@ def ocr_core(image_path):
     return text
 
 # Example usage
-image_path = r'.\img\Capture1.PNG'
+# image_path = r'.\img\Capture1.PNG'
+image_path = r'C:\\Users\\914476\\Documents\\Github\\OCR\\venv\\img\\Capture1.PNG'
+image_path = os.path.abspath(image_path)
 extracted_text = ocr_core(image_path)
 print(extracted_text)
